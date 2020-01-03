32b93ec37669c57c10dd7fa08a8ff433adc7970b
Paul Pan <panjunhong05@gmail.com>
2020-01-02 23:56:46 -0800

    Removed template boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'typeface-nunito';
+import './styles/index.less';
+
+const App: React.FC = () => {
+  return (
+      <h1>Welcome to ACM!</h1>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
